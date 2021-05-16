3c52cbfd1078012ecfb8eaed32a2671288924ea5
rajesh <rajesh.nitc@gmail.com>
2021-05-16 14:50:58 +0530

    cloudbuild artifactory access with impersonation

--- 0-bootstrap/provider.tf
+++ 0-bootstrap/provider.tf
@@ -1,3 +1,7 @@
 provider "google" {
   impersonate_service_account = "org-terraform@prj-b-seed-6949.iam.gserviceaccount.com"
 }
+
+provider "google-beta" {
+  impersonate_service_account = "org-terraform@prj-b-seed-6949.iam.gserviceaccount.com"
+}
