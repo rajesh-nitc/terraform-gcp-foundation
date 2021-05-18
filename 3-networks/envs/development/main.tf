78b5c75c5ad4558e4e9f47de072eaccf9f073a44
rajesh <rajesh.nitc@gmail.com>
2021-05-18 15:52:03 +0530

    tf plan on 3-networks

--- 3-networks/envs/development/main.tf
+++ 3-networks/envs/development/main.tf
@@ -17,8 +17,8 @@
 locals {
   environment_code          = "d"
   env                       = "development"
-  restricted_project_id     = data.google_projects.restricted_host_project.projects[0].project_id
-  restricted_project_number = data.google_project.restricted_host_project.number
+  # restricted_project_id     = data.google_projects.restricted_host_project.projects[0].project_id
+  # restricted_project_number = data.google_project.restricted_host_project.number
   base_project_id           = data.google_projects.base_host_project.projects[0].project_id
   parent_id                 = var.parent_folder != "" ? "folders/${var.parent_folder}" : "organizations/${var.org_id}"
   mode                      = var.enable_hub_and_spoke ? "spoke" : null
@@ -79,13 +79,13 @@ data "google_active_folder" "env" {
   VPC Host Projects
 *****************************************/
 
-data "google_projects" "restricted_host_project" {
-  filter = "parent.id:${split("/", data.google_active_folder.env.name)[1]} labels.application_name=restricted-shared-vpc-host labels.environment=${local.env} lifecycleState=ACTIVE"
-}
+# data "google_projects" "restricted_host_project" {
+#   filter = "parent.id:${split("/", data.google_active_folder.env.name)[1]} labels.application_name=restricted-shared-vpc-host labels.environment=${local.env} lifecycleState=ACTIVE"
+# }
 
-data "google_project" "restricted_host_project" {
-  project_id = data.google_projects.restricted_host_project.projects[0].project_id
-}
+# data "google_project" "restricted_host_project" {
+#   project_id = data.google_projects.restricted_host_project.projects[0].project_id
+# }
 
 data "google_projects" "base_host_project" {
   filter = "parent.id:${split("/", data.google_active_folder.env.name)[1]} labels.application_name=base-shared-vpc-host labels.environment=${local.env} lifecycleState=ACTIVE"
@@ -94,57 +94,57 @@ data "google_projects" "base_host_project" {
 /******************************************
  Restricted shared VPC
 *****************************************/
-module "restricted_shared_vpc" {
-  source                           = "../../modules/restricted_shared_vpc"
-  project_id                       = local.restricted_project_id
-  project_number                   = local.restricted_project_number
-  environment_code                 = local.environment_code
-  access_context_manager_policy_id = var.access_context_manager_policy_id
-  restricted_services              = ["bigquery.googleapis.com", "storage.googleapis.com"]
-  members                          = ["serviceAccount:${var.terraform_service_account}"]
-  private_service_cidr             = local.restricted_private_service_cidr
-  org_id                           = var.org_id
-  parent_folder                    = var.parent_folder
-  bgp_asn_subnet                   = local.bgp_asn_number
-  default_region1                  = var.default_region1
-  default_region2                  = var.default_region2
-  domain                           = var.domain
-  windows_activation_enabled       = var.windows_activation_enabled
-  dns_enable_inbound_forwarding    = var.dns_enable_inbound_forwarding
-  dns_enable_logging               = var.dns_enable_logging
-  firewall_enable_logging          = var.firewall_enable_logging
-  optional_fw_rules_enabled        = var.optional_fw_rules_enabled
-  nat_enabled                      = var.nat_enabled
-  nat_bgp_asn                      = var.nat_bgp_asn
-  nat_num_addresses_region1        = var.nat_num_addresses_region1
-  nat_num_addresses_region2        = var.nat_num_addresses_region2
-  folder_prefix                    = var.folder_prefix
-  mode                             = local.mode
+# module "restricted_shared_vpc" {
+#   source                           = "../../modules/restricted_shared_vpc"
+#   project_id                       = local.restricted_project_id
+#   project_number                   = local.restricted_project_number
+#   environment_code                 = local.environment_code
+#   access_context_manager_policy_id = var.access_context_manager_policy_id
+#   restricted_services              = ["bigquery.googleapis.com", "storage.googleapis.com"]
+#   members                          = ["serviceAccount:${var.terraform_service_account}"]
+#   private_service_cidr             = local.restricted_private_service_cidr
+#   org_id                           = var.org_id
+#   parent_folder                    = var.parent_folder
+#   bgp_asn_subnet                   = local.bgp_asn_number
+#   default_region1                  = var.default_region1
+#   default_region2                  = var.default_region2
+#   domain                           = var.domain
+#   windows_activation_enabled       = var.windows_activation_enabled
+#   dns_enable_inbound_forwarding    = var.dns_enable_inbound_forwarding
+#   dns_enable_logging               = var.dns_enable_logging
+#   firewall_enable_logging          = var.firewall_enable_logging
+#   optional_fw_rules_enabled        = var.optional_fw_rules_enabled
+#   nat_enabled                      = var.nat_enabled
+#   nat_bgp_asn                      = var.nat_bgp_asn
+#   nat_num_addresses_region1        = var.nat_num_addresses_region1
+#   nat_num_addresses_region2        = var.nat_num_addresses_region2
+#   folder_prefix                    = var.folder_prefix
+#   mode                             = local.mode
 
-  subnets = [
-    {
-      subnet_name           = "sb-${local.environment_code}-shared-restricted-${var.default_region1}"
-      subnet_ip             = local.restricted_subnet_primary_ranges[var.default_region1]
-      subnet_region         = var.default_region1
-      subnet_private_access = "true"
-      subnet_flow_logs      = var.subnetworks_enable_logging
-      description           = "First ${local.env} subnet example."
-    },
-    {
-      subnet_name           = "sb-${local.environment_code}-shared-restricted-${var.default_region2}"
-      subnet_ip             = local.restricted_subnet_primary_ranges[var.default_region2]
-      subnet_region         = var.default_region2
-      subnet_private_access = "true"
-      subnet_flow_logs      = var.subnetworks_enable_logging
-      description           = "Second ${local.env} subnet example."
-    }
-  ]
-  secondary_ranges = {
-    "sb-${local.environment_code}-shared-restricted-${var.default_region1}" = local.restricted_subnet_secondary_ranges[var.default_region1]
-  }
-  allow_all_ingress_ranges = local.enable_transitivity ? local.restricted_hub_subnet_ranges : null
-  allow_all_egress_ranges  = local.enable_transitivity ? local.restricted_subnet_aggregates : null
-}
+#   subnets = [
+#     {
+#       subnet_name           = "sb-${local.environment_code}-shared-restricted-${var.default_region1}"
+#       subnet_ip             = local.restricted_subnet_primary_ranges[var.default_region1]
+#       subnet_region         = var.default_region1
+#       subnet_private_access = "true"
+#       subnet_flow_logs      = var.subnetworks_enable_logging
+#       description           = "First ${local.env} subnet example."
+#     },
+#     {
+#       subnet_name           = "sb-${local.environment_code}-shared-restricted-${var.default_region2}"
+#       subnet_ip             = local.restricted_subnet_primary_ranges[var.default_region2]
+#       subnet_region         = var.default_region2
+#       subnet_private_access = "true"
+#       subnet_flow_logs      = var.subnetworks_enable_logging
+#       description           = "Second ${local.env} subnet example."
+#     }
+#   ]
+#   secondary_ranges = {
+#     "sb-${local.environment_code}-shared-restricted-${var.default_region1}" = local.restricted_subnet_secondary_ranges[var.default_region1]
+#   }
+#   allow_all_ingress_ranges = local.enable_transitivity ? local.restricted_hub_subnet_ranges : null
+#   allow_all_egress_ranges  = local.enable_transitivity ? local.restricted_subnet_aggregates : null
+# }
 
 /******************************************
  Base shared VPC
--- 3-networks/envs/development/outputs.tf
+++ 3-networks/envs/development/outputs.tf
@@ -18,50 +18,50 @@
  Restricted Outputs
 *********************/
 
-output "restricted_host_project_id" {
-  value       = local.restricted_project_id
-  description = "The restricted host project ID"
-}
+# output "restricted_host_project_id" {
+#   value       = local.restricted_project_id
+#   description = "The restricted host project ID"
+# }
 
-output "restricted_network_name" {
-  value       = module.restricted_shared_vpc.network_name
-  description = "The name of the VPC being created"
-}
+# output "restricted_network_name" {
+#   value       = module.restricted_shared_vpc.network_name
+#   description = "The name of the VPC being created"
+# }
 
-output "restricted_network_self_link" {
-  value       = module.restricted_shared_vpc.network_self_link
-  description = "The URI of the VPC being created"
-}
+# output "restricted_network_self_link" {
+#   value       = module.restricted_shared_vpc.network_self_link
+#   description = "The URI of the VPC being created"
+# }
 
-output "restricted_subnets_names" {
-  value       = module.restricted_shared_vpc.subnets_names
-  description = "The names of the subnets being created"
-}
+# output "restricted_subnets_names" {
+#   value       = module.restricted_shared_vpc.subnets_names
+#   description = "The names of the subnets being created"
+# }
 
-output "restricted_subnets_ips" {
-  value       = module.restricted_shared_vpc.subnets_ips
-  description = "The IPs and CIDRs of the subnets being created"
-}
+# output "restricted_subnets_ips" {
+#   value       = module.restricted_shared_vpc.subnets_ips
+#   description = "The IPs and CIDRs of the subnets being created"
+# }
 
-output "restricted_subnets_self_links" {
-  value       = module.restricted_shared_vpc.subnets_self_links
-  description = "The self-links of subnets being created"
-}
+# output "restricted_subnets_self_links" {
+#   value       = module.restricted_shared_vpc.subnets_self_links
+#   description = "The self-links of subnets being created"
+# }
 
-output "restricted_subnets_secondary_ranges" {
-  value       = module.restricted_shared_vpc.subnets_secondary_ranges
-  description = "The secondary ranges associated with these subnets"
-}
+# output "restricted_subnets_secondary_ranges" {
+#   value       = module.restricted_shared_vpc.subnets_secondary_ranges
+#   description = "The secondary ranges associated with these subnets"
+# }
 
-output "restricted_access_level_name" {
-  value       = module.restricted_shared_vpc.access_level_name
-  description = "Access context manager access level name"
-}
+# output "restricted_access_level_name" {
+#   value       = module.restricted_shared_vpc.access_level_name
+#   description = "Access context manager access level name"
+# }
 
-output "restricted_service_perimeter_name" {
-  value       = module.restricted_shared_vpc.service_perimeter_name
-  description = "Access context manager service perimeter name"
-}
+# output "restricted_service_perimeter_name" {
+#   value       = module.restricted_shared_vpc.service_perimeter_name
+#   description = "Access context manager service perimeter name"
+# }
 
 /******************************************
  Private Outputs
