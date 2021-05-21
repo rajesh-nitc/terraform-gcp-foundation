366cfb1277b196373f2722ab5ed304b32cfd73d5
rajesh <rajesh.nitc@gmail.com>
2021-05-21 17:10:16 +0530

    added network grp and default prj sa roles

--- 1-org/envs/shared/groups.tf
+++ 1-org/envs/shared/groups.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "google_organization_iam_member" "group_network_admins" {
-  for_each = toset(local.group_network_admins)
+  for_each = toset(local.group_network_admins_roles)
   org_id   = var.org_id
   role     = each.key
   member   = "group:${var.group_network_admins}"
