c42c73e6d241de46237688c6050e8125acfd4a85
rajesh <rajesh.nitc@gmail.com>
2022-01-08 03:04:27 +0530

    chore:dataflow fw rule in separate file

--- 03-networks/modules/base_shared_vpc/firewall.tf
+++ 03-networks/modules/base_shared_vpc/firewall.tf
@@ -54,32 +54,6 @@ resource "google_compute_firewall" "allow_private_api_egress" {
   target_tags = ["allow-google-apis", "dataflow"]
 }
 
-resource "google_compute_firewall" "allow_dataflow_internal" {
-  name      = "fw-${var.environment_code}-shared-base-1000-i-a-dataflow-internal"
-  network   = module.main.network_name
-  project   = var.project_id
-  direction = "INGRESS"
-  priority  = 65534
-
-  dynamic "log_config" {
-    for_each = var.firewall_enable_logging == true ? [{
-      metadata = "INCLUDE_ALL_METADATA"
-    }] : []
-
-    content {
-      metadata = log_config.value.metadata
-    }
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["12345", "12346"]
-  }
-
-  source_tags = ["dataflow"]
-  target_tags = ["dataflow"]
-}
-
 /******************************************
   Optional firewall rules
  *****************************************/
---
+++ 03-networks/modules/base_shared_vpc/firewall_dataflow.tf
@@ -0,0 +1,25 @@
+resource "google_compute_firewall" "allow_dataflow_internal" {
+  name      = "fw-${var.environment_code}-shared-base-1000-i-a-dataflow-internal"
+  network   = module.main.network_name
+  project   = var.project_id
+  direction = "INGRESS"
+  priority  = 65534
+
+  dynamic "log_config" {
+    for_each = var.firewall_enable_logging == true ? [{
+      metadata = "INCLUDE_ALL_METADATA"
+    }] : []
+
+    content {
+      metadata = log_config.value.metadata
+    }
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["12345", "12346"]
+  }
+
+  source_tags = ["dataflow"]
+  target_tags = ["dataflow"]
+}
