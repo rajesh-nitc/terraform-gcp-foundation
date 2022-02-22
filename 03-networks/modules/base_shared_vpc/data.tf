d930cb80e6cbbb3cd848ae1b3c6c90fed3dd86ec
rajesh <rajesh.nitc@gmail.com>
2022-02-22 12:37:07 +0530

    chore:chore

--- 03-networks/modules/base_shared_vpc/data.tf
+++ 03-networks/modules/base_shared_vpc/data.tf
@@ -1,11 +1,14 @@
+# 35.191.0.0/16, 209.85.152.0/22, 209.85.204.0/22
 data "google_netblock_ip_ranges" "legacy_health_checkers" {
   range_type = "legacy-health-checkers"
 }
 
+# 35.191.0.0/16, 130.211.0.0/22
 data "google_netblock_ip_ranges" "health_checkers" {
   range_type = "health-checkers"
 }
 
+# 35.235.240.0/20
 data "google_netblock_ip_ranges" "iap_forwarders" {
   range_type = "iap-forwarders"
 }
